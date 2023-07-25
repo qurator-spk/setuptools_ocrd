4914c7186bb71de7ec3fbe4da4964da663d0c1b2
Mike Gerber <mike.gerber@sbb.spk-berlin.de>
2023-07-25 13:45:27 +0200

    🎨 Warn if setuptools is old
    If setuptools is < 61, give a warning. Unfortunately pip hides this,
    unless using -v...
    
    Closes gh-2.

--- setuptools_ocrd/_integration/setuptools.py
+++ setuptools_ocrd/_integration/setuptools.py
@@ -1,8 +1,17 @@
 import logging
+import warnings
+import setuptools
 from setuptools_ocrd import _get_version
 
 
 log = logging.getLogger(__name__)
+
+
+def _warn_on_old_setuptools(_version = setuptools.__version__):
+    if int(_version.split(".")[0]) < 61:
+        warnings.warn(RuntimeWarning(f"setuptools=={_version} is old!"))
+
+_warn_on_old_setuptools()
 
 def infer_version(dist):
     log.debug("infer_version: %r", vars(dist.metadata))
